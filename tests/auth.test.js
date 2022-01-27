7c2638a1eebbde5f432fee84a523d8155a4a6f04
Antoine-Rzepa <antoine.rzepa@le-campus-numerique.fr>
2022-01-27 09:52:51 +0100

    test 24

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -6,13 +6,15 @@ describe("Auth features", () => {
     let page;
 
     test('login and logout', async () => {
+
+        await page.goto(process.env.TESTED_WEBSITE);
         await page.waitForSelector('#login_button_container');
         await page.type('#user-name', process.env.TEST_LOGIN);
         await page.type('#password', process.env.TEST_PASSWORD);
 
         await Promise.all([
             page.waitForNavigation(),
-            page.click('[name="login-button"]'),
+            page.click('#login-button'),
         ]);
 
         //await page.click('[name="login-button"]')
